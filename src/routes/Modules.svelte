742e0f1e530681c23d8d2cbc269d54dd9d707430
Teun van Lith <teun_van_lith@hotmail.com>
2022-08-28 19:09:13 +0200

    commit 19:09

--- src/routes/Modules.svelte
+++ src/routes/Modules.svelte
@@ -26,48 +26,45 @@
 </script>
 <div class="containerModules">
     {#if moduleA}
-    <div transition:fade class="containerModule" style="border: 2px solid {colorA}">
+    <div transition:fade class="containerModule" style="border: 3px solid {colorA}">
         <h1 style="color:{colorA}">Games</h1>
-        <div class="infoModule">
         <h2>Space Invaders</h2><br />
         <h2>Snake</h2>
-        </div>
-
+        <div class="containerButton">
         <Button {text} on:click={ () => {dispatch('showModuleA')}}/>
+        </div>
     </div>
     {/if}
 
     {#if moduleB}
-    <div transition:fade class="containerModule" style="border: 2px solid {colorB}">
+    <div transition:fade class="containerModule" style="border: 3px solid {colorB}">
         <h1 style="color:{colorB}">Work</h1>
-        <div class="infoModule">
         <h2>Tip 'n Top Reviews</h2>
-        </div>
-
+        <div class="containerButton">
         <Button {text} on:click={ () => {dispatch('showModuleB')}}/>
+        </div>
+    </div>
+    {/if}
+
+       {#if moduleD}
+    <div transition:fade class="containerModule" style="border: 3px solid {colorD}">
+        <h1 style="color:{colorD}">Tools</h1>
+        <h2>Calculator</h2><br />
+        <h2>To do</h2><br />
+        <h2>Clock</h2><br />
+        <div class="containerButton">
+        <Button {text} on:click={ () => {dispatch('showModuleD')}}/>
+        </div>
     </div>
     {/if}
 
     {#if moduleC}
-    <div transition:fade class="containerModule" style="border: 2px solid {colorC}">
-        <h1 style="color:{colorC}">Webdesign</h1>
-        <div class="infoModule">
+    <div transition:fade class="containerModule" style="border: 3px solid {colorC}">
+        <h1 style="color:{colorC}">About me</h1>
         <h2>Contact me</h2>
-        </div>
-
+        <div class="containerButton">
         <Button {text} on:click={ () => {dispatch('showModuleC')}}/>
-    </div>
-    {/if}
-
-    {#if moduleD}
-    <div transition:fade class="containerModule" style="border: 2px solid {colorD}">
-        <h1 style="color:{colorD}">Tools</h1>
-        <div class="infoModule">
-        <h2>Calculator</h2><br />
-        <h2>To do</h2><br />
-        <h2>Clock</h2><br />
         </div>
-        <Button {text} on:click={ () => {dispatch('showModuleD')}}/>
     </div>
     {/if}
 </div>
@@ -96,17 +93,38 @@
 
     h1 {
         margin-top: 50px;
-        margin-bottom: 280px;
+        margin-bottom: 50px;
     }
 
     h2 {
         margin-bottom: 0px;
     }
 
-    .infoModule {
+    .containerButton {
         position: absolute;
-        top: 150px;
-        text-align: center;
-        width: 100%;
+        bottom: 20px;
+        width: 300px;
+            
     }
+
+    @media (max-width: 900px) {
+        .containerModule {
+        width: 300px;
+        height: 300px;
+        border-radius: 10px;
+        background-color: rgba(31, 31, 31, .5);
+        border: 1px solid rgb(31, 31, 31);
+        position: relative;
+        margin: 10px 10px;
+    }
+        h1 {
+            margin-top: 20px;
+            margin-bottom: 20px;
+        }
+
+        h2 {
+            font-size: 16px;
+        }
+    }
+
 </style>
